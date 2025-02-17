06af1f029fa228ca3355aea966ee22924e876985
Nikola Zrnic <zrnic.nikola@outlook.com>
2025-02-17 17:56:50 +0100

    made some min change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Include this if using React
 
 export default defineConfig({
-  base: '/QUIZ/', // Replace <REPO_NAME> with your GitHub repository name
+  base:'/QUIZ/', // Replace <REPO_NAME> with your GitHub repository name
   plugins: [react()] // Include this line if using React
 });
